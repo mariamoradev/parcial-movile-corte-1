37dded72bdcea33a70e6e7e570528e2e60248299
mariamoradev <mariainfo150704@gmail.com>
2024-08-24 21:41:40 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'parcial_corte1_movile',
+  webDir: 'www'
+};
+
+export default config;
